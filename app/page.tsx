ccfe7abf37cec5c4315154df87451b179e21a9fe
Shina Qin <bqshina@gmail.com>
2025-05-18 21:06:42 +1000

    fix unUsed component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import CreatedGallery from "./components/CreatedGallery";
-import PreviewImage from "./components/PreviewImage";
 import PromptArea from "./components/PromptArea";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center pt-8">
       <CreatedGallery />
-      {/* <PreviewImage /> */}
       <PromptArea />
     </div>
   );
